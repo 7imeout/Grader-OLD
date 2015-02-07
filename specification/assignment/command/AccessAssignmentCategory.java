09bd0a28dc0db380c759fe85ec8197ff26f97f9b
mbleifer <mbleifer@89017a09-e8da-471d-b09c-d6420b0e5526>
2015-02-07 03:23:33 +0000

    Javadocs done
    git-svn-id: svn+ssh://unix13.csc.calpoly.edu/home/doryu/projects/SVN/grader/trunk@110 89017a09-e8da-471d-b09c-d6420b0e5526

--- specification/assignment/command/AccessAssignmentCategory.java
+++ specification/assignment/command/AccessAssignmentCategory.java
@@ -3,5 +3,8 @@ package assignment.command;
 import assignment.AssignmentCategory;
 import command.ReadCommand;
 
+/**
+ * Gets the assignment category from the model.
+ */
 public abstract class AccessAssignmentCategory implements ReadCommand<AssignmentCategory> {
 }
