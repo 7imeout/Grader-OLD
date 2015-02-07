cc4cba14fbeafec0f2683b8db01abce83c408e21
mbleifer <mbleifer@89017a09-e8da-471d-b09c-d6420b0e5526>
2015-02-07 03:23:50 +0000

    Javadocs done
    git-svn-id: svn+ssh://unix13.csc.calpoly.edu/home/doryu/projects/SVN/grader/trunk@112 89017a09-e8da-471d-b09c-d6420b0e5526

--- specification/assignment/command/AccessAssignmentSubmission.java
+++ specification/assignment/command/AccessAssignmentSubmission.java
@@ -3,5 +3,8 @@ package assignment.command;
 import assignment.AssignmentSubmission;
 import command.ReadCommand;
 
+/**
+ * Gets the assignment submission from the model.
+ */
 public abstract class AccessAssignmentSubmission implements ReadCommand<AssignmentSubmission> {
 }
