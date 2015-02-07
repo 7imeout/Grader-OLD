b6d1365b5cc5bb1b2c92011b3426a817ef2867d3
mmurph18 <mmurph18@89017a09-e8da-471d-b09c-d6420b0e5526>
2015-02-07 05:39:02 +0000

    added a remove course user command
    git-svn-id: svn+ssh://unix13.csc.calpoly.edu/home/doryu/projects/SVN/grader/trunk@134 89017a09-e8da-471d-b09c-d6420b0e5526

--- specification/user/student/command/RemoveStudent.java
+++ specification/user/student/command/RemoveStudent.java
@@ -2,11 +2,12 @@ package user.student.command;
 
 import command.WriteCommand;
 import course.CourseModifier;
+import user.command.RemoveCourseUser;
 
 /**
  * RemoveStudent implements WriteCommand to remove student data from a course
  * spreadsheet by overwriting the data.
  *                                                                           <p>
  */
-public abstract class RemoveStudent implements WriteCommand<CourseModifier> {
+public abstract class RemoveStudent extends RemoveCourseUser {
 }
