b85cd71cc52341db1c9d381bab6f20c33d500364
mbleifer <mbleifer@89017a09-e8da-471d-b09c-d6420b0e5526>
2015-02-07 05:06:20 +0000

    Javadocs done
    git-svn-id: svn+ssh://unix13.csc.calpoly.edu/home/doryu/projects/SVN/grader/trunk@123 89017a09-e8da-471d-b09c-d6420b0e5526

--- specification/course/command/AccessLatePolicy.java
+++ specification/course/command/AccessLatePolicy.java
@@ -3,5 +3,8 @@ package course.command;
 import command.ReadCommand;
 import course.LatePolicy;
 
+/**
+ * Gets the late policy from the model.
+ */
 public abstract class AccessLatePolicy implements ReadCommand<LatePolicy> {
 }
