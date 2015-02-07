79686b6f21b649a8ca475d38ac0c35b483599379
mbleifer <mbleifer@89017a09-e8da-471d-b09c-d6420b0e5526>
2015-02-07 03:23:43 +0000

    Javadocs done
    git-svn-id: svn+ssh://unix13.csc.calpoly.edu/home/doryu/projects/SVN/grader/trunk@111 89017a09-e8da-471d-b09c-d6420b0e5526

--- specification/assignment/command/AccessAssignmentGrade.java
+++ specification/assignment/command/AccessAssignmentGrade.java
@@ -3,5 +3,8 @@ package assignment.command;
 import assignment.AssignmentGrade;
 import command.ReadCommand;
 
+/**
+ * Gets the assignment grade from the model.
+ */
 public abstract class AccessAssignmentGrade implements ReadCommand<AssignmentGrade> {
 }
