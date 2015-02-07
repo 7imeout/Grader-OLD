e9a4a3a104279200c00a2b76903bcb13c0c4e657
mbleifer <mbleifer@89017a09-e8da-471d-b09c-d6420b0e5526>
2015-02-07 05:00:53 +0000

    Javadocs done
    git-svn-id: svn+ssh://unix13.csc.calpoly.edu/home/doryu/projects/SVN/grader/trunk@122 89017a09-e8da-471d-b09c-d6420b0e5526

--- specification/course/command/AccessGradeSchema.java
+++ specification/course/command/AccessGradeSchema.java
@@ -3,5 +3,8 @@ package course.command;
 import command.ReadCommand;
 import course.GradeSchema;
 
+/**
+ * Gets the grade schema from the model.
+ */
 public abstract class AccessGradeSchema implements ReadCommand<GradeSchema> {
 }
