ea1e6705e731e57f607b11e98e7dd4c8aa4c3185
Michael Murphy <mmurph18@calpoly.edu>
2015-03-18 15:08:27 -0700

    added comments to Identity.java

--- specification/admin/Identity.java
+++ specification/admin/Identity.java
@@ -3,7 +3,15 @@ package admin;
 /**
  * Represents a login name (i.e. a Cal Poly username), and its domain (i.e. Cal Poly).
  */
-public interface Identity {
-   String getName();
-   String getDomain();
+public abstract class Identity {
+    
+    /**
+     * The login name (i.e. a Cal Poly username)
+     */
+   String name;
+
+    /**
+     * The domain for this identity (i.e. Cal Poly)
+     */
+   String domain;
 }
